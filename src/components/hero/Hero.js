be9ac43eec226642ad55a3188f07ab4dca913f2f
Eryk Szułczyński <eryk.szulczynski0@gmail.com>
2020-04-10 14:52:58 +0200

    Linked stylesheets for Navbar and Hero

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from './hero.module.scss'
 
 export default function Hero() {
     return (
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from './navbar.module.scss'
 
 export default function Navbar() {
     return (
