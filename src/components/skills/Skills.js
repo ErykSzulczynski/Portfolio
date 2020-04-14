288cd9dbaed6701773253251ec77d6be797b4a30
Eryk Szułczyński <eryk.szulczynski0@gmail.com>
2020-04-14 15:05:05 +0200

    Skills section structure and basic styling

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -4,7 +4,20 @@ import styles from './skills.module.scss'
 export default function Skills() {
     return (
         <div className={styles.skills}>
-            Skills
+           <div className={styles.skills__title}>
+                Skills
+           </div>
+           <div className={styles.skills__grid}>
+                <div className={styles.skills__gridItem}>
+                    <p className={styles.skills__gridItemTitle}>Ive been working with</p>
+                </div>
+                <div className={styles.skills__gridItem}>
+                <p className={styles.skills__gridItemTitle}>Ive been working with</p>
+                </div>
+                <div className={styles.skills__gridItem}>
+                <p className={styles.skills__gridItemTitle}>Ive been working with</p>
+                </div>
+           </div>
         </div>
     )
 }
