14d2e2c79082da65c59c45ba47dc42358b4f5854
Eryk Szułczyński <eryk.szulczynski0@gmail.com>
2020-04-27 12:45:21 +0200

    Logo implementation

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import styles from './navbar.module.scss'
-import logo from './Brzoza.png'
+import React from "react"
+import styles from "./navbar.module.scss"
+import logo from "./Brzoza.png"
 
 export default function Navbar() {
-    return (
-        <div className={styles.navbar}>
-           <ul className={styles.links__list}>
-               {/*<li className={styles.links__listItem}><img src={logo} className={styles.logo} alt="logo"/></li>*/}
-               <li className={styles.links__listItem}>About</li>
-               <li className={styles.links__listItem}>Skills</li>
-               <li className={styles.links__listItem}>Portfolio</li>
-               <li className={styles.links__listItem}>Contact</li>
-           </ul>
-        </div>
-    )
+  return (
+    <div className={styles.navbar}>
+      <img src={logo} className={styles.logo} alt="logo" />
+      <ul className={styles.links__list}>
+        <li className={styles.links__listItem}>About</li>
+        <li className={styles.links__listItem}>Skills</li>
+        <li className={styles.links__listItem}>Portfolio</li>
+        <li className={styles.links__listItem}>Contact</li>
+      </ul>
+    </div>
+  )
 }
