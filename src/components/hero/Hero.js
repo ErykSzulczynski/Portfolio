04a29698649fbcff700410bdc20875ca39bce91f
Eryk Szułczyński <eryk.szulczynski0@gmail.com>
2020-08-17 09:23:23 +0200

    Fixed hero section links

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -7,17 +7,24 @@ export default function Hero() {
 
     let link = 'https://www.google.com'
 
+    const elo = () => {
+        console.log("elo")
+    }
+
     return (
             <div className={styles.hero}>
-                <p className={styles.hero__text}>
+                <div className={styles.hero__text}>
                     <span className={styles.hero__textHighlighted}>
                         Eryk Szułczyński
                     </span><br/>
                     Web developer
                     <br/>
-                    <span className={styles.hero__icon}><FontAwesomeIcon icon={faGithubSquare} /></span>
-                    <span className={styles.hero__icon}><FontAwesomeIcon icon={faLinkedin} /></span>
-                </p> 
+                    
+                </div>
+                <div className={styles.hero__links}>
+                    <span className={styles.hero__icon} onClick={() => {window.location.assign("https://github.com/ErykSzulczynski")}}><FontAwesomeIcon icon={faGithubSquare} /></span>
+                    <span className={styles.hero__icon} onClick={() => {window.location.assign("https://www.linkedin.com/in/eryk-szu%C5%82czy%C5%84ski-225386192/")}}><FontAwesomeIcon icon={faLinkedin} /></span>
+                </div>
                 <div className={styles.box}></div>
             </div>
     )
